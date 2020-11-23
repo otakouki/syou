7d4d0b62450c4a5f18a91a6c1fedb39019eaf345
SANO_ <2190403@ecccomp.com>
2020-11-23 16:08:28 +0900

    Web Android

---
+++ test4/app/src/main/java/com/ese2a_2190403/event02xample/test/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ese2a_2190403.event02xample.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("http://syou.sakura.ne.jp/smp_api.php");
+    }
+}
